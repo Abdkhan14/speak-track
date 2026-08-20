8b666a93afbcb20967fcbec171a062c385407446
Abdullah Khan <abdullahibnekhan@gmail.com>
2026-08-20 13:21:52 -0400

    (speaker-mode): Adding speaker mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import SpeechEditor from './components/SpeechEditor'
+import Teleprompter from './components/Teleprompter'
 
-const Title = styled.h1`
-  font-family: sans-serif;
-  font-size: 2.5rem;
-  font-weight: 600;
-  color: #1a1a1a;
-  margin: 0 0 1.5rem;
-`
+type Mode = 'edit' | 'speaker'
 
 const Page = styled.div`
   display: flex;
@@ -19,11 +15,35 @@ const Page = styled.div`
   box-sizing: border-box;
 `
 
+const Title = styled.h1`
+  font-family: sans-serif;
+  font-size: 2.5rem;
+  font-weight: 600;
+  color: #1a1a1a;
+  margin: 0 0 1.5rem;
+`
+
 export default function App() {
+  const [mode, setMode] = useState<Mode>('edit')
+  const [speakerText, setSpeakerText] = useState('')
+
+  function handleStartSpeaking(text: string) {
+    setSpeakerText(text)
+    setMode('speaker')
+  }
+
+  function handleBack() {
+    setMode('edit')
+  }
+
+  if (mode === 'speaker') {
+    return <Teleprompter text={speakerText} onBack={handleBack} />
+  }
+
   return (
     <Page>
       <Title>Speak Track</Title>
-      <SpeechEditor />
+      <SpeechEditor onStartSpeaking={handleStartSpeaking} />
     </Page>
   )
 }
