290271eab42fdcf54f8420a90c7f9c66f7fcd73a
Abdullah Khan <abdullahibnekhan@gmail.com>
2026-08-20 12:24:12 -0400

    (textarea): Adding textareawith count and max char limit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import styled from 'styled-components'
+import SpeechEditor from './components/SpeechEditor'
 
 const Title = styled.h1`
   font-family: sans-serif;
   font-size: 2.5rem;
   font-weight: 600;
   color: #1a1a1a;
-  margin: 0;
+  margin: 0 0 1.5rem;
 `
 
 const Page = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+  padding: 3rem 1.5rem;
+  height: 100vh;
   background: #ffffff;
+  box-sizing: border-box;
 `
 
 export default function App() {
   return (
     <Page>
       <Title>Speak Track</Title>
+      <SpeechEditor />
     </Page>
   )
 }
